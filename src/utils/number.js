d16356012b3213f49c223e91abdd35da21a574ea
EsioFreitas <esio.gustavo@gmail.com>
2021-06-10 17:18:43 +0200

    feat: start project
    Co-authored-by: AnaCarolcs <ancarolcs@gmail.com>

--- src/utils/number.js
+++
@@ -1,5 +0,0 @@
-const addLeadingZero = (num, places) => String(num).padStart(places, '0');
-
-const padTo2Digits = (num) => num.toString().padStart(2, "0");
-
-export { addLeadingZero, padTo2Digits };
