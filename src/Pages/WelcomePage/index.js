fa22105f6e3078ff35dc57805fc3a173fdc3daee
EsioFreitas <esio.gustavo@gmail.com>
2021-11-20 17:18:43 +0200

    feat: add column
    Co-authored-by: AnaCarolcs <ancarolcs@gmail.com>

--- src/Pages/WelcomePage/index.js
+++ src/Pages/WelcomePage/index.js
@@ -2,17 +2,17 @@ import React from "react";
 import { Box, VStack, Text } from "@chakra-ui/layout";
 import { useNavigate } from "react-router-dom";
 import { Button } from "@chakra-ui/button";
-import { Center } from '@chakra-ui/react'
+import { Center } from "@chakra-ui/react";
 
 export default function WelcomePage() {
-    let navigate = useNavigate();
+  let navigate = useNavigate();
 
-    const title = `
+  const title = `
         Kort: Ferramenta Educacional para Gestão de Fluxo de Trabalho Orientada 
         à Metodologia Kanban.
     `;
 
-    const description = `
+  const description = `
         O Método Kanban propõe um olhar mais ativo e construtivo para a resolução 
         de tarefas e administração de demandas. Atualmente, essa abordagem pode ser 
         aplicada atravez de aplicações web que facilita o seu uso. No entanto, 
@@ -22,32 +22,36 @@ export default function WelcomePage() {
         tem o intuito de apresentar como essas mecánicas acontecem.
     `;
 
-    return (
-        <Box height="100vh" width="100vw">
-            <Center height="100vh" width="100vw">
-                <VStack
-                    overflow="scroll"
-                    spacing={100}
-                    align="center"
-                    justifyContent="center"
-                    width="100%"
-                    height="100%"
-                >
-                    <Text textAlign="center" fontSize="6xl">{title}</Text>
-                    <Text textAlign="center" fontSize="lg" width="70%">{description}</Text>
-                    <Button
-                        colorScheme="facebook"
-                        width="60%"
-                        maxWidth={233}
-                        onClick={() => navigate(`/main-page`)}
-                        fontSize="1.5rem"
-                        fontWeight={700}
-                        height={50}
-                    >
-                        Começar
-                    </Button>
-                </VStack>
-            </Center>
-        </Box>
-    );
+  return (
+    <Box height="100vh" width="100vw">
+      <Center height="100vh" width="100vw">
+        <VStack
+          overflow="scroll"
+          spacing={100}
+          align="center"
+          justifyContent="center"
+          width="100%"
+          height="100%"
+        >
+          <Text textAlign="center" fontSize="6xl">
+            {title}
+          </Text>
+          <Text textAlign="center" fontSize="lg" width="70%">
+            {description}
+          </Text>
+          <Button
+            colorScheme="facebook"
+            width="60%"
+            maxWidth={233}
+            onClick={() => navigate(`/main-page`)}
+            fontSize="1.5rem"
+            fontWeight={700}
+            height={50}
+          >
+            Começar
+          </Button>
+        </VStack>
+      </Center>
+    </Box>
+  );
 }
