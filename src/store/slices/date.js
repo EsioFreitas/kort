11c4aa430b53cca1b1fe3c6b0def00fb59d0a0b4
EsioFreitas <esio.gustavo@gmail.com>
2021-11-29 17:18:43 +0200

    fix: refact date slices
    Co-authored-by: AnaCarolcs <ancarolcs@gmail.com>

--- src/store/slices/date.js
+++ src/store/slices/date.js
@@ -1,19 +1,19 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice } from "@reduxjs/toolkit";
 
 export const dateSlice = createSlice({
-  name: 'date',
+  name: "date",
   initialState: {
-    date: new Date()
+    date: new Date(),
   },
   reducers: {
     addDay: (state, action) => {
       const newDate = new Date(state.date.getTime());
       newDate.setDate(newDate.getDate() + 1);
-      state.date = newDate
+      state.date = newDate;
     },
   },
-})
+});
 
-export const { addDay } = dateSlice.actions
+export const { addDay } = dateSlice.actions;
 
-export default dateSlice.reducer
+export default dateSlice.reducer;
