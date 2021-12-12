8d91f7627783e22b6c18c0d64db75402f316f1e0
EsioFreitas <esio.gustavo@gmail.com>
2021-12-12 17:18:43 +0200

    feat: add logic number
    Co-authored-by: AnaCarolcs <ancarolcs@gmail.com>

--- src/utils/number.js
+++ src/utils/number.js
@@ -1,4 +1,4 @@
-const addLeadingZero = (num, places) => String(num).padStart(places, '0');
+const addLeadingZero = (num, places) => String(num).padStart(places, "0");
 
 const padTo2Digits = (num) => num.toString().padStart(2, "0");
 
