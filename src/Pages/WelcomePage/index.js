9c9eab9dd763b2809057850f7bb1ac103ed78d84
EsioFreitas <esio.gustavo@gmail.com>
2022-03-20 17:18:43 +0200

    feat: add new column
    Co-authored-by: AnaCarolcs <ancarolcs@gmail.com>

--- src/Pages/WelcomePage/index.js
+++ src/Pages/WelcomePage/index.js
@@ -7,20 +7,19 @@ import { Center } from "@chakra-ui/react";
 export default function WelcomePage() {
   let navigate = useNavigate();
 
-  const title = `
-        Kort: Ferramenta Educacional para Gestão de Fluxo de Trabalho Orientada 
-        à Metodologia Kanban.
-    `;
-
   const description = `
-        O Método Kanban propõe um olhar mais ativo e construtivo para a resolução 
-        de tarefas e administração de demandas. Atualmente, essa abordagem pode ser 
-        aplicada atravez de aplicações web que facilita o seu uso. No entanto, 
-        essa abordagem evoluiu com o tempo ao ponto de virar uma métodologia, 
-        assim, tendo suas próprias práticas e métricas. Essa ferramenta, 
-        baseada nas práticas e métricas da Metodologia Kanban, 
-        tem o intuito de apresentar como essas mecánicas acontecem.
-    `;
+  O Método Kanban propõe um olhar mais ativo e construtivo para a resolução 
+  de tarefas e administração de demandas. Atualmente, essa abordagem pode ser 
+  aplicada atravez de aplicações web que facilita o seu uso. No entanto, 
+  essa abordagem evoluiu com o tempo ao ponto de virar uma métodologia, 
+  assim, tendo suas próprias práticas e métricas. Essa ferramenta, 
+  baseada nas práticas e métricas da Metodologia Kanban, 
+  tem o intuito de apresentar como essas mecánicas acontecem.
+`;
+  const title = `
+  Kort: Ferramenta Educacional para Gestão de Fluxo de Trabalho Orientada 
+  à Metodologia Kanban.
+  `;
 
   return (
     <Box height="100vh" width="100vw">
